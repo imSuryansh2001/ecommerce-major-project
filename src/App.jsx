666d981173ac21c496dfe4f8d2ca70212f9fc249
Molik Singla <moliksingla02@gmail.com>
2024-02-05 11:15:39 +0530

    setup react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,35 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
+    // Suryansh ------------------------------------
     {
         path: "/",
         element: <section>Home Page!</section>,
+    },
+    // Prayag ------------------------------------
+    {
+        path: "/product",
+        element: <section>Product Page!</section>,
+
+        children: [
+            {
+                path: "/:id",
+                element: <section>Single Product Detail Page!</section>,
+            },
+        ],
+    },
+    // Molik ------------------------------------
+    {
+        path: "/login",
+        element: <section>Login Page!</section>,
+    },
+    {
+        path: "/cart",
+        element: <section>Cart Page!</section>,
+    },
+    {
+        path: "/checkout",
+        element: <section>Checkout Page!</section>,
     },
 ]);
 
