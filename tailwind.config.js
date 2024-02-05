752de4ce39df298f561acd9c469e2d743383fbdf
Molik Singla <moliksingla02@gmail.com>
2024-02-05 15:09:34 +0530

    initialize font and primary-colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "primary-gradient": "linear-gradient(75deg,#0098ff_0%,#7b35c8_100%)",
+            },
+            fontFamily: {
+                primary: ["Outfit", "sans-serif"],
+            },
+        },
     },
     plugins: [],
 };
